ac2f0169f0ba6648c7a21e3e04193796fc59521f
j721 <juliadvu@gmail.com>
2020-03-20 11:25:59 -0500

    Changes to app.js and components file

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,16 @@
 import React, {useState, useEffect} from 'react';
-import './App.css';
+import Card from './components/Card';
+import styled from 'styled-components';
 import axios from 'axios';
+import './App.css';
+
+
+const Wrapper =styled.div`
+display:flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: center;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -13,18 +23,33 @@ const App = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
       axios.get('https://swapi.co/api/people/')
-	.then(response  => {
+	    .then(response  => {
         setData(response.data.results);
-        console.log(data);
+        console.log(data, 'data was returned');
       })
       .catch(error => {
-        console.log("data failed to return", error);
+        console.log(error);
       });
   }, [data]);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Wrapper>
+        {data.map(character=>{
+          return(
+            <Card>
+              key={character.name}
+              name={character.name}
+              mass={character.mass}
+              hair_color={character.hair_color}
+              skin_color ={character.skin_color} 
+              birth_year = {character.birth_year}
+              gender = {character.gender}
+            </Card>
+          );
+        })}
+      </Wrapper>
     </div>
   );
 }
